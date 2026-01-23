69c8f2e295960c3ca5d65190ea11a9bd7c82aba8
Ryan Esteves <snargledorf@users.noreply.github.com>
2026-01-23 10:33:34 -0500

    Move IsUserDefined to extension type and bump version
    Moved the IsUserDefined property from TokenType to a new TokenTypeExtensions file.
    Bumped version to 4.1.0.

--- Tokensharp/TokenType.cs
+++ Tokensharp/TokenType.cs
@@ -6,10 +6,4 @@ public abstract record TokenType<T>(string Identifier)
     public static readonly T Text = T.Create("text");
     public static readonly T Number = T.Create("number");
     public static readonly T WhiteSpace = T.Create("whitespace");
-
-    private bool? _isUserDefined;
-    public bool IsUserDefined
-    {
-        get { return _isUserDefined ??= this != Text && this != Number && this != WhiteSpace; }
-    }
 }
