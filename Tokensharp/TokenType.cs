d37a3e2ef5fa02d2990f82903ae53c5e05bfd53d
Ryan Esteves <snargledorf@users.noreply.github.com>
2026-05-14 16:06:35 -0400

    Add auto incrementing index for tokens (#57)
    * ```
    feat(Tokensharp): add implicit conversion for TokenType and bump version to 4.7.0-beta.1
    
    - Introduce implicit conversion of `TokenType` to `int` for improved usability.
    - Bump project version to `4.7.0-beta.1` to reflect changes and enable pre-release versioning.
    ```
    
    * feat(Tokensharp.Tests): add unit tests for token indexing
    
    - Introduce `IndexingTests` to verify proper ordering of token indices across multiple `TokenType` implementations.
    - Add test cases to ensure independent token index counters for different `TokenType` types.
    
    * fix(Tokensharp): update token indexing to start from 0
    
    - Adjust default `_nextTokenTypeIndex` initialization to `-1` to ensure indices start at 0.
    - Update `IndexingTests` to reflect the new indexing logic.
    - Ensure independent counters for different `TokenType` implementations start at 0.

--- Tokensharp/TokenType.cs
+++ Tokensharp/TokenType.cs
@@ -3,7 +3,16 @@ namespace Tokensharp;
 public abstract record TokenType<T>(string Identifier)
     where T : TokenType<T>, ITokenType<T>
 {
+    // ReSharper disable once StaticMemberInGenericType
+    private static int _nextTokenTypeIndex = -1;
+    private readonly int _tokenTypeIndex = Interlocked.Increment(ref _nextTokenTypeIndex);
+    
     public static readonly T Text = T.Create("text");
     public static readonly T Number = T.Create("number");
     public static readonly T WhiteSpace = T.Create("whitespace");
+
+    public static implicit operator int(TokenType<T> tokenType)
+    {
+        return tokenType._tokenTypeIndex;
+    }
 }
